2f243f1f1b7b18f65f7465f2eb6a844dbc382cc5
Tijunel <justintijunel@gmail.com>
2020-03-20 16:55:31 -0600

    Added some setup

---
+++ Middleware/auth.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+
+const withAuth = (req, res, next) => {
+    const token = req.body.token ||
+        req.query.token ||
+        req.headers['x-access-token'] ||
+        req.cookies.token;
+
+    if (!token) return res.status(401).json({ message: "Authentification Error" });
+    else {
+        jwt.verify(token, "Secret", (err, decoded) => {
+            if (err) res.status(401).send('Unauthorized, Invalid token');
+            else {
+                req.forum = decoded.forum;
+                next();
+            }
+        })
+    }
+};
+
+module.exports = withAuth;
--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-//Starting point
+'use strict';
+
+//Imports
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+//Setup
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(session({
+    secret: 'xCufvwEyu14Tuu7l',
+    resave: true,
+    saveUninitialized: true,
+    secure: true
+}));
+app.use(cookieParser());
+
+//Add paths
+//For example:
+//const teamMember = require('./teamMember');
+//app.use('/api/teamMember', teamMember);
+
+app.listen(PORT, () => {
+    console.log(`Server Listening on Port ${PORT}`)
+});
