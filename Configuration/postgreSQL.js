e8ffca713c27e425a0e68794d4674d0f455a77ef
Tijunel <justintijunel@gmail.com>
2020-03-21 16:04:07 -0600

    Added nodemon dependancy

--- Configuration/postgreSQL.js
+++ Configuration/postgreSQL.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const { Pool, Client } = require('pg');
+
+PostGreSQL = new Pool({
+    port: 5432,
+    password: 'greentomato',
+    database: 'postgres',
+    host: '', //Our host
+    user: 'postgres',
+});
+
+module.exports = PostGreSQL;
