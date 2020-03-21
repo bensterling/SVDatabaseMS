5c99107d27055a938b4daf009f6a72d5266f77c5
Tijunel <justintijunel@gmail.com>
2020-03-21 17:07:41 -0600

    Set up index

--- server.js
+++ server.js
@@ -20,11 +20,29 @@ app.use(session({
 }));
 app.use(cookieParser());
 
-//Add paths
-//For example:
-//const teamMember = require('./teamMember');
-//app.use('/api/teamMember', teamMember);
+//Import Routes
+const driver = require('./Routes/driver');
+const log = require('./Routes/log');
+const race = require('./Routes/race');
+const sensor = require('./Routes/sensor');
+const subteam = require('./Routes/subteam');
+const team = require('./Routes/team');
+const teamMember = require('./Routes/teamMember');
+const vehicle = require('./Routes/vehicle');
+const vehicleSpec = require('./Routes/vehicleSpec');
 
+//Add Routes
+app.use('/driver', driver);
+app.use('/log', log);
+app.use('/race', race);
+app.use('/sensor', sensor);
+app.use('/subteam', subteam);
+app.use('/team', team);
+app.use('/teamMember', teamMember);
+app.use('/vehicle', vehicle);
+app.use('/vehicleSpec', vehicleSpec);
+
+//Start Server
 app.listen(PORT, () => {
     console.log(`Server Listening on Port ${PORT}`)
 });
