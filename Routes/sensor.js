065fa51400e15c6da9e49947ea02978f36ffbb2a
ZacharyKahn16 <zackkahn16@gmail.com>
2020-04-09 21:08:38 -0600

    Added sanitization using Joi.

--- Routes/sensor.js
+++ Routes/sensor.js
@@ -4,60 +4,141 @@ const database = require("../Configuration/postgreSQL");
 const express = require("express");
 const withAnyAuth = require("../Middleware/auth")[0];
 const withAdminAuth = require("../Middleware/auth")[1];
+const Joi = require("@hapi/joi");
+const apiKeySchema = require("../Middleware/sanitizeAuth");
+const sanitizeInputs = require("./helperFunctions");
 const sensor = express.Router();
 
 sensor.get("/getSensors", withAnyAuth, async (req, res) => {
-	//Validate the request
+  //Validate the request
+  // TODO: Uncomment and test once get sensors is implemented.
+  // const result = await sanitizeInputs(req, res, apiKeySchema);
+  // if (result < 0) {
+  //   console.error("Error: API key did not meet input requirements.");
+  //   return;
+  // }
+  //Request:
+  //VehicleId
+  database
+    .func()
+    .then(data => {})
+    .catch(error => {});
+  //Response:
+  //List of all sensors for the given vehicle
+});
 
-	//Request:
-	//VehicleId
-	database
-		.func()
-		.then(data => { })
-		.catch(error => { });
-	//Response:
-	//List of all sensors for the given vehicle
+const putSensorOutputUnitSchema = Joi.object({
+  sensorId: Joi.number()
+    .integer()
+    .required(),
+  vehicleId: Joi.number()
+    .integer()
+    .required(),
+  outputUnit: Joi.string()
+    .alphanum()
+    .required()
 });
 
 sensor.put("/putSensorOutputUnit", withAdminAuth, async (req, res) => {
-	//Validate the request
+  //Validate the request
+  const result = await sanitizeInputs(
+    req,
+    res,
+    apiKeySchema,
+    putSensorOutputUnitSchema
+  );
 
-	//Execute the stored procedure
-	database.proc("putSensorOutputUnit",
-		[
-			req.user.APIKey,
-			req.body.sensorId,
-			req.body.vehicleId,
-			req.body.outputUnit
-		])
-		.then(data => {
-			res.status(200).send("Success!").end();
-		})
-		.catch(error => {
-			res.status(500).send("Error!").end();
-		});
+  if (result < 0) {
+    if (result === -1) {
+      console.error("Error: API key did not meet input requirements.");
+    } else {
+      console.error(
+        "Error: One of inputs in body did not meet input requirements."
+      );
+    }
+    return;
+  }
+  //Execute the stored procedure
+  database
+    .proc("putSensorOutputUnit", [
+      req.user.APIKey,
+      req.body.sensorId,
+      req.body.vehicleId,
+      req.body.outputUnit
+    ])
+    .then(data => {
+      res
+        .status(200)
+        .send("Success!")
+        .end();
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .send("Error!")
+        .end();
+    });
+});
+
+const postSensorSchema = Joi.object({
+  vehicleId: Joi.number()
+    .integer()
+    .required(),
+  name: Joi.string().required(),
+  outputUnit: Joi.string()
+    .alphanum()
+    .required(),
+  category: Joi.string()
+    .alphanum()
+    .required(),
+  lowerBound: Joi.number()
+    .integer()
+    .required(),
+  upperBound: Joi.number()
+    .integer()
+    .required()
 });
 
 sensor.post("/postSensor", withAdminAuth, async (req, res) => {
-	//Validate the request
+  //Validate the request
+  const result = await sanitizeInputs(req, res, apiKeySchema, postSensorSchema);
 
-	//Execute the stored procedure
-	database.proc("postSensor",
-		[
-			req.user.APIKey,
-			req.body.vehicleId,
-			req.body.name,
-			req.body.outputUnit,
-			req.body.category,
-			req.body.lowerBound,
-			req.body.upperBound
-		])
-		.then(data => {
-			res.status(200).send("Success!").end();
-		})
-		.catch(error => {
-			res.status(500).send("Error!").end();
-		});
+  if (result < 0) {
+    if (result === -1) {
+      console.error("Error: API key did not meet input requirements.");
+    } else {
+      console.error(
+        "Error: One of inputs in body did not meet input requirements."
+      );
+    }
+    return;
+  }
+
+  //Execute the stored procedure
+  database
+    .proc("postSensor", [
+      req.user.APIKey,
+      req.body.vehicleId,
+      req.body.name,
+      req.body.outputUnit,
+      req.body.category,
+      req.body.lowerBound,
+      req.body.upperBound
+    ])
+    .then(data => {
+      res
+        .status(200)
+        .send("Success!")
+        .end();
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .send("Error!")
+        .end();
+    });
 });
 
 module.exports = sensor;
--- Routes/team.js
+++ Routes/team.js
@@ -8,64 +8,93 @@ const withAnyAuth = require("../Middleware/auth")[0];
 const team = express.Router();
 
 team.get("/getAllTeams", withAnyAuth, async (req, res) => {
-    //Validate the request
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            errors: errors.array()
-        }).end();
-    }
-    //Execute the stored function
-    database.func("getAllTeams")
-        .then(data => {
-            res.status(200).json(data[0].getAllTeams).end();
-        })
-        .catch(error => {
-            res.status(500).send("Error!").end();
-        });
+  //Validate the request
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({
+        errors: errors.array()
+      })
+      .end();
+  }
+  //Execute the stored function
+  database
+    .func("getAllTeams")
+    .then(data => {
+      res
+        .status(200)
+        .json(data[0].getAllTeams)
+        .end();
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .send("Error!")
+        .end();
+    });
 });
 
 team.get("/getTeam", withAnyAuth, async (req, res) => {
-    //Validate the request
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            errors: errors.array()
-        }).end();
-    }
-    //Execute the stored function
-    database.func("getTeam", req.user.APIKey)
-        .then(data => {
-            res.status(200).json(data[0].getTeam).end();
-        })
-        .catch(error => {
-            res.status(500).send("Error!").end();
-        });
+  //Validate the request
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({
+        errors: errors.array()
+      })
+      .end();
+  }
+  //Execute the stored function
+  database
+    .func("getTeam", req.user.APIKey)
+    .then(data => {
+      res
+        .status(200)
+        .json(data[0].getTeam)
+        .end();
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .send("Error!")
+        .end();
+    });
 });
 
 team.post("/postTeam", async (req, res) => {
-    //Validate the request
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            errors: errors.array()
-        }).end();
-    }
-    //Execute the stored procedure
-    const apiKey = (uuidAPIKey.create()).apiKey;
-    database.proc("postTeam",
-        [
-            req.body.name,
-            req.body.location,
-            req.body.university,
-            apiKey
-        ])
-        .then(data => {
-            res.status(200).send("Success! The API Key for this team is: " + apiKey).end();
-        })
-        .catch(error => {
-            res.status(500).send("Error!").end();
-        });
+  //Validate the request
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({
+        errors: errors.array()
+      })
+      .end();
+  }
+  //Execute the stored procedure
+  const apiKey = uuidAPIKey.create().apiKey;
+  database
+    .proc("postTeam", [
+      req.body.name,
+      req.body.location,
+      req.body.university,
+      apiKey
+    ])
+    .then(data => {
+      res
+        .status(200)
+        .send("Success! The API Key for this team is: " + apiKey)
+        .end();
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .send("Error!")
+        .end();
+    });
 });
 
 module.exports = team;
